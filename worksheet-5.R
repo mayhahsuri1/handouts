9c2ed477008352cc03fbbbde107883233b4327c0
Mayhah <msuri1@umd.edu>
2017-08-02 19:11:31 +0000

    lessons completed during SESYNC

--- worksheet-5.R
+++ worksheet-5.R
@@ -1,36 +1,51 @@
 ## Getting started
 
 library(dplyr)
-library(...)
-animals <- read.csv(..., na.strings = '') %>%
+library(ggplot2)
+animals <- read.csv(data/animals.csv, na.strings = '') %>%
+  filter(!is.na(species_id), !is.na(sex), !is.na(weight))
+
+library(dplyr)
+library(ggplot2)
+animals <- read.csv("data/animals.csv", na.strings = "") %>%
   filter(!is.na(species_id), !is.na(sex), !is.na(weight))
 
 ## Constructing layered graphics in ggplot
 
-ggplot(...,
-       ...) +
-  ...
+ggplot(data = animals,
+       aes(x = species_id, y = weight)) +
+  geom_point()
+  
+ggplot(data = animals,
+       aes(x = species_id, y = weight)) +
+  geom_boxplot()
 
 ggplot(data = animals,
        aes(x = species_id, y = weight)) +
-  ...
-
-ggplot(data = animals,
-       aes(x = species_id, y = weight)) +
-  geom_boxplot() ...
-  geom_point(...,
-             ...,
-             ...)
-
-ggplot(data = animals,
-       aes(x = species_id, y = weight, ...)) +
   geom_boxplot() +
-  geom_point(stat = 'summary',
-             fun.y = 'mean')
+  geom_point(stat = "summary",
+             fun.y = "mean",
+             color = "red")
+
+ggplot(data = animals,
+       aes(x = species_id, y = weight, color = species_id)) +
+  geom_boxplot() +
+  geom_point(stat = "summary",
+             fun.y = "mean")
+
+animals_dm <- filter(animals, species_id == 'DM')
+ggplot(data = animals_dm,
+       aes(x = year, y = weight, color = sex)) +
+  geom_line(stat = 'summary',
+            fun.y = 'mean')
 
 ## Exercise 1
 
-...
+animals_dm <- filter(animals, species_id == 'DM')
+ggplot(data = animals_dm,
+       aes(x = year, y = weight, color = sex)) +
+  geom_line(stat = 'summary',
+            fun.y = 'mean')
 
 ## Adding a regression line
 
@@ -74,22 +89,58 @@ year_wgt <- ggplot(data = animals_dm,
              fun.y = 'mean') +
   geom_smooth(method = 'lm')
 
+animals_dm <- filter(animals, species_id == 'DM')
+ggplot(data = animals_dm,
+       aes(x = year, y = weight)) + 
+  geom_point(aes(shape = sex),
+             size = 3,
+             stat = 'summary',
+             fun.y = 'mean') +
+  geom_smooth(method = 'lm')
+ggplot(data = animals_dm,
+       aes(x = year, y = weight)) + 
+  geom_point(aes(shape = sex),
+             size = 3,
+             stat = 'summary',
+             fun.y = 'mean') +
+  geom_smooth(aes(group = sex), method = 'lm')
+ggplot(data = animals_dm,
+       aes(x = year,
+           y = weight,
+           color = sex)) + 
+  geom_point(aes(shape = sex),
+             size = 3,
+             stat = 'summary',
+             fun.y = 'mean') +
+  geom_smooth(method = 'lm')
+year_wgt <- ggplot(data = animals_dm,
+                   aes(x = year,
+                       y = weight,
+                       color = sex)) + 
+  geom_point(aes(shape = sex),
+             size = 3,
+             stat = "summary",
+             fun.y = "mean") +
+  geom_smooth(method = "lm")
+
 year_wgt +
-  ...
-                     
+  scale_color_manual(values = c("darkblue", "orange"))
 year_wgt <- year_wgt +
-  scale_color_manual(...)
+  scale_color_manual(values = c("black", "red"))
 year_wgt
-
 ## Exercise 2
+ggplot(data=animals_dm, aes(x=weight, fill=sex)) +
+  geom_histogram(binwidth=1)
 
-...
+?geom_histogram
+
 
 ## Axes, labels and themes
 
 histo <- ggplot(data = animals_dm,
                 aes(x = weight, fill = sex)) +
-  geom_...
+  geom_histogram(binwidth=3, color = "white")
+
 histo
 
 histo <- histo +
@@ -103,18 +154,18 @@ histo
 histo <- histo +
   theme_bw() +
   theme(legend.position = c(0.2, 0.5),
-        plot.title = ...,
-        ... = element_text(...),
-        ... = element_text(size = 13, vjust = 0))
+        plot.title = element_text(face="bold, vjust=2"),
+        axis.title.x = element_text(size=13, vjust=1),
+        axis.title.y = element_text(size = 13, vjust = 0))
 histo
 
 ## Facets
 
-animals_common <- filter(animals, ...)
-ggplot(data = ...,
-       ...) +
+animals_common <- filter(animals, species_id %in% c("DM", "PP", "DO"))
+ggplot(data = animals_common,
+       aes(x=weight)) +
   geom_histogram() +
-  ...
+  facet_wrap(~species_id) +
   labs(title = "Weight of most common species",
        x = "Count",
        y = "Weight (g)")
